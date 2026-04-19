857c5aee6660ef12864c5141dd6046404b553688
Rainiel Montaril <rmontaril3@gmail.com>
2026-04-20 02:13:08 +0800

    Updated2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/My-Online-Portfolio/',
+    base: '/Online-Portfolio/',
     plugins: [
       react(),
       tailwindcss(),
